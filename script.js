63200807df67d5fbfdad6c52c1e12fe06eb9081b
KrzysiekLu <krzysztof.luuczak@gmail.com>
2022-07-21 14:28:31 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const navbar = document.querySelector(".navbar");
 const btnOpenNavbar = document.querySelector(".btn-open-navbar");
 const sections = document.querySelectorAll(".section");
@@ -5,9 +7,9 @@ const sectionsContent = document.querySelectorAll(".section__content");
 const btnMoveDown = document.querySelector(".main-header__btn-move-down");
 const btnChangeSectionNext = document.querySelector(".btn-next");
 const btnChangeSectionPrev = document.querySelector(".btn-prev");
+const changeSectionBtns = document.querySelectorAll(".btn-change-section");
 
 //Scroll to first section
-
 btnMoveDown.addEventListener("click", () => {
   document.querySelector(".section-1").scrollIntoView({
     behavior: "smooth",
@@ -16,78 +18,72 @@ btnMoveDown.addEventListener("click", () => {
 });
 
 // Open Navbar
-
 const openNavbar = () => {
   navbar.classList.toggle("show-navbar");
   navbar.classList.contains("show-navbar")
     ? (btnOpenNavbar.textContent = "Close Menu")
     : (btnOpenNavbar.textContent = "Open Menu");
 };
-
 btnOpenNavbar.addEventListener("click", openNavbar);
 
 // observer;
-
 const options = {
   threshold: 0.5,
   rootMargin: "100px",
 };
 
 // Fade in animation observer
-
+let currentSection = -1;
 const sectionsObserver = new IntersectionObserver((entries) => {
+  let [watchedSection] = entries;
+
+  if (watchedSection.isIntersecting)
+    currentSection = watchedSection.target.dataset.index;
+  console.log(currentSection);
+
   entries.forEach((entry) => {
     entry.target.children[0].classList.toggle(
       "show-section-content",
       entry.isIntersecting
     );
-    // if (entry.isIntersecting) sectionsObserver.unobserve(entry.target);
   });
+  showHideChangeSectionBtns();
 }, options);
 
 sections.forEach((section) => {
   sectionsObserver.observe(section);
 });
 
-//Change section observer
-
-let currentSection = 0;
-const observerForBtns = new IntersectionObserver((entries) => {
-  let [watchedSection] = entries;
-
-  if (watchedSection.isIntersecting)
-    currentSection = watchedSection.target.dataset.index;
-  console.log(currentSection);
-});
-
-sections.forEach(
-  (section) => {
-    observerForBtns.observe(section);
-  },
-  {
-    threshold: 1,
-    rootMargin: "0px",
-  }
-);
-
-const changeSectionBtns = document.querySelectorAll(".btn-change-section");
-
 const changeSection = (e) => {
-  if (e.target.classList.contains("btn-next") && currentSection != 10) {
+  if (
+    e.target.parentElement.classList.contains("btn-next") &&
+    currentSection != 10
+  ) {
     currentSection++;
-
     document
       .querySelector(`.section-${currentSection}`)
       .scrollIntoView({ behavior: "smooth", block: "center" });
   }
-  if (e.target.classList.contains("btn-prev") && currentSection != 1) {
+  if (
+    e.target.parentElement.classList.contains("btn-prev") &&
+    currentSection != -1
+  ) {
     currentSection--;
     document
       .querySelector(`.section-${currentSection}`)
       .scrollIntoView({ behavior: "smooth", block: "center" });
   }
 };
-
 changeSectionBtns.forEach((btn) =>
-  btn.addEventListener("click", (e) => changeSection(e))
+  btn.addEventListener("click", (e) => {
+    changeSection(e);
+  })
 );
+
+//hide buttons
+
+const showHideChangeSectionBtns = () => {
+  +currentSection >= 0
+    ? changeSectionBtns.forEach((btn) => (btn.style.display = "block"))
+    : changeSectionBtns.forEach((btn) => (btn.style.display = "none"));
+};
